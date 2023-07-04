7e59890fefd5bfd5eccef1aa4788004fdf153cf1
Rubycode <rubyruby3423@gmail.com>
2023-07-05 01:48:54 +0530

    yes

---
+++ script.js
@@ -0,0 +1,17 @@
+function getRandomColor() {
+    // Function code goes here
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for(var i = 0; i<6 ; i++){
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+
+  document.getElementById('button-bg').addEventListener('click' , function(){
+    var randomColor = getRandomColor();
+    document.body.style.backgroundColor = randomColor;
+  });
+
+  
